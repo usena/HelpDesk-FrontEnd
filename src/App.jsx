4839a5db636607047666f7951c8985fd669b01dc
GooGaaGee <viriya.s@ligercambodia.org>
2025-06-05 09:14:04 +0700

    switch

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Routes, Route } from "react-router";
 import { Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
-import Home from './components/Customer/home';
-//import Home from './components/Staff/home';
-import Login from './components/login';
+//import Home from './components/Customer/home';
+import Home from './components/Staff/home';
+//import Login from './components/login';
 import EmailVerify from './components/EmailVerify';
 
 function App() {
